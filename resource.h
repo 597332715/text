ddfc91f7166ba1b787327f4e6fe813c214ae6e7c
ximeng <597332715@qq.com>
2018-10-24 22:05:21 +0800

    Four operation interface edition

---
+++ resource.h
@@ -0,0 +1,23 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 arithmetic.rc 使用
+//
+#define IDR_MENU1                       101
+#define ID_40001                        40001
+#define IDM_Integer                       40002
+#define IDM_Fraction                        40003
+#define IDM_Factorial                        40004
+#define IDM_Background                        40005
+#define IDM_Explain                       40006
+#define IDM_Exit                       40007
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40005
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
